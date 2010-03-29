f938a6cb07a76597856eb12d36c63eb44d477ff7
cory.powers <cory.powers@922e40f0-fdfd-11de-9d47-03d50c098576>
2010-03-29 00:48:27 +0000

    Added new files

---
+++ trunk/MediaItem.h
@@ -0,0 +1,46 @@
+//
+//  MediaItem.h
+//  TranscodingMachine
+//
+//  Created by Cory Powers on 3/22/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+#define ItemTypeTV 1
+#define ItemTypeMovie 2
+
+@class QueueItem;
+
+
+@interface MediaItem :  NSManagedObject  
+{
+}
+
+@property (nonatomic, retain) NSString * showName;
+@property (nonatomic, retain) NSString * releaseDate;
+@property (nonatomic, retain) NSString * message;
+@property (nonatomic, retain) NSString * output;
+@property (nonatomic, retain) NSNumber * type;
+@property (nonatomic, retain) NSData * coverArt;
+@property (nonatomic, retain) NSNumber * episode;
+@property (nonatomic, retain) NSNumber * season;
+@property (nonatomic, retain) NSString * input;
+@property (nonatomic, retain) NSNumber * hdVideo;
+@property (nonatomic, retain) NSString * longDescription;
+@property (nonatomic, retain) NSString * summary;
+@property (nonatomic, retain) NSString * network;
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * copyright;
+@property (nonatomic, retain) NSString * genre;
+
+@property (nonatomic, retain) QueueItem * queueItem;
+
+@property (readonly) NSString *shortName;
+@property (readonly) NSString *episodeId;
+
+@end
+
+
+
---
+++ trunk/MediaItem.m
@@ -0,0 +1,51 @@
+// 
+//  MediaItem.m
+//  TranscodingMachine
+//
+//  Created by Cory Powers on 3/22/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "MediaItem.h"
+
+
+@implementation MediaItem 
+
+@dynamic showName;
+@dynamic releaseDate;
+@dynamic message;
+@dynamic output;
+@dynamic type;
+@dynamic coverArt;
+@dynamic episode;
+@dynamic season;
+@dynamic input;
+@dynamic hdVideo;
+@dynamic longDescription;
+@dynamic summary;
+@dynamic network;
+@dynamic title;
+@dynamic copyright;
+@dynamic genre;
+
+@dynamic queueItem;
+
+-(NSString *)shortName{
+	NSString *shortName = [[self input] lastPathComponent];
+	if ([[self type] intValue] == ItemTypeTV && ![[self showName] isEqualToString:@""]) {
+		shortName = [NSString stringWithFormat:@"%@ S%.2dE%.2d - %@", [self showName], [[self season] intValue], [[self episode] intValue], [self title]];
+	}else if([[self type] intValue] == ItemTypeMovie && ![[self title] isEqualToString:@""]) {
+		shortName = [self title];
+	}
+	
+	return shortName;
+}
+
+-(NSString *)episodeId{
+	if ([self type] == [NSNumber numberWithInt:ItemTypeTV]) {
+		return [NSString stringWithFormat:@"%d%.2d", [[self season] intValue], [[self episode] intValue]];
+	}
+	return [NSString string];
+}
+
+@end
